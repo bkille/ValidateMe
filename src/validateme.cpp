8322006e51b23e7ae5dee8b1725e17bfaeb25c7e
Bryce Lorenz Kille <brycekille@gmail.com>
2021-11-26 16:04:22 -0600

    working MAF validation

--- src/validateme.cpp
+++ src/validateme.cpp
@@ -8,6 +8,8 @@
 #include <string>
 #include <string.h>
 #include <algorithm>
+#include <memory>
+#include <regex>
 #include <unordered_map>
 #include <map>
 #include <unordered_set>
@@ -65,7 +67,8 @@ int main(int argc, char** argv){
             "truth-file",
             truth_file,
             "Truth MSA file"
-    )->required()->check(CLI::ExistingFile);
+    //)->required()->check(CLI::ExistingFile);
+    );
     app.add_flag(
             "-v,--verbose",
             verbose,
@@ -91,68 +94,110 @@ int main(int argc, char** argv){
     //omp_set_num_threads(num_threads);
     
 
-
-    // Set up truth data-structures
     seqan3::sequence_file_input<gapped_traits> truth_file_h{truth_file};
-    std::unordered_map<unsigned int, std::set<seqloc>> idx_to_set; 
-    std::map<seqloc, unsigned int> loc_to_idx; 
-    int alignment_length = 0;
+    int curr_block = -1;
+    std::vector<std::shared_ptr<std::set<seqloc>>> groups; 
+    std::map<seqloc, std::shared_ptr<std::set<seqloc>>> loc_to_idx; 
     for (auto & [seq, id, qual] : truth_file_h)
     {
-        seqan3::debug_stream << "ID:     " << id << '\n';
-        if (alignment_length && seq.size() != alignment_length) {
-            spdlog::error("Sequences do not have the same length!");
-            return 1;
+        //seqan3::debug_stream << "ID:     " << id << '\n';
+        std::string s = id;
+        std::regex rgx("(.+?) ID=(\\d+?)\\s(\\d+?)\\s(\\d+?)\\s([+,-])\\s(\\d+*)");
+        std::smatch matches;
+        std::string seq_id;
+        int start, strand, block_id;
+        if(std::regex_search(s, matches, rgx)) {
+            seq_id = matches[1].str();
+            block_id = std::stoi(matches[2].str());
+            start = std::stoi(matches[3].str());
+            strand = matches[5].str() == "-" ? -1 : 1;
+        } else {
+            std::cout << "Match not found\n";
+            continue;
         }
-        alignment_length = seq.size();
-
+        if (block_id != curr_block) {
+            curr_block = block_id;
+            groups = std::vector<std::shared_ptr<std::set<seqloc>>>(seq.size());
+            for (auto i = 0; i < seq.size(); ++i) {
+                groups[i] = std::make_shared<std::set<seqloc>>();
+            }
+        }
         unsigned int ng = 0;
         for (auto col_idx = 0; col_idx < seq.size(); ++col_idx) {
             if (seq[col_idx] == seqan3::gap{})
                 continue;
             ++ng;
-            idx_to_set[col_idx].insert(std::make_pair(id, ng));
-            loc_to_idx[std::make_pair(id, ng)] = col_idx;
+            seqloc sl = std::make_pair(seq_id, start + ng);
+            groups[col_idx]->insert(sl);
+            loc_to_idx[sl] = groups[col_idx];
+            //seqan3::debug_stream << *loc_to_idx[std::make_pair(seq_id, start + strand*ng)] << std::endl;
         }
-        //seqan3::debug_stream << query_group << std::endl;
     }
 
-
-    // Set up query data structures
     seqan3::sequence_file_input<gapped_traits> query_file_h{query_file};
-    std::vector<std::set<seqloc>> query_groups(alignment_length);
+    curr_block = -1;
+    std::vector<std::shared_ptr<std::vector<std::set<seqloc>>>> block_groups;
+    std::shared_ptr<std::vector<std::set<seqloc>>> query_groups; 
     for (auto & [seq, id, qual] : query_file_h)
     {
-        seqan3::debug_stream << "QID:     " << id << '\n';
-        if (alignment_length && seq.size() != alignment_length) {
-            spdlog::error("Query sequence length does not match the truth!");
-            return 1;
+        //seqan3::debug_stream << "ID:     " << id << '\n';
+        std::string s = id;
+        std::regex rgx("(.+?) ID=(\\d+?)\\s(\\d+?)\\s(\\d+?)\\s([+,-])\\s(\\d+*)");
+        std::smatch matches;
+        std::string seq_id;
+        int start, strand, block_id;
+        if(std::regex_search(s, matches, rgx)) {
+            seq_id = matches[1].str();
+            block_id = std::stoi(matches[2].str());
+            start = std::stoi(matches[3].str());
+            strand = matches[5].str() == "-" ? -1 : 1;
+        } else {
+            std::cout << "Match not found\n";
+            continue;
+        }
+        if (block_id != curr_block) {
+            //score block
+            //
+            //
+            if (curr_block != -1) {
+                block_groups.push_back(query_groups);
+            }
+            //wipe block data structures
+            curr_block = block_id;
+            query_groups = std::make_shared<std::vector<std::set<seqloc>>>(seq.size());
         }
         unsigned int ng = 0;
         for (auto col_idx = 0; col_idx < seq.size(); ++col_idx) {
             if (seq[col_idx] == seqan3::gap{})
                 continue;
             ++ng;
-            query_groups[col_idx].insert(std::make_pair(id, ng));
+            (*query_groups)[col_idx].insert(std::make_pair(seq_id, start + ng));
         }
     }
 
 
     // Compute intersections
     unsigned int fp = 0, fn = 0, tp = 0;
-    for (auto query_group : query_groups) {
-        for (seqloc sl : query_group) {
-            auto truth_group = idx_to_set[loc_to_idx[sl]];
-            std::vector<seqloc> v_intersection;
-            std::set_intersection(truth_group.begin(), truth_group.end(), query_group.begin(), query_group.end(), std::back_inserter(v_intersection));
-            tp += v_intersection.size();
-            v_intersection.clear();
-            std::set_difference(truth_group.begin(), truth_group.end(), query_group.begin(), query_group.end(), std::back_inserter(v_intersection));
-            fn += v_intersection.size();
-            v_intersection.clear();
-            std::set_difference(query_group.begin(), query_group.end(), truth_group.begin(), truth_group.end(), std::back_inserter(v_intersection));
-            fp += v_intersection.size();
-            v_intersection.clear();
+    for (auto block_group : block_groups) {
+        for (auto query_group : *block_group) {
+            //seqan3::debug_stream << query_group << std::endl;
+            for (seqloc sl : query_group) {
+                auto truth_group = *loc_to_idx[sl];
+                std::vector<seqloc> v_intersection;
+                std::set_intersection(truth_group.begin(), truth_group.end(), query_group.begin(), query_group.end(), std::back_inserter(v_intersection));
+                tp += v_intersection.size();
+                v_intersection.clear();
+                std::set_difference(truth_group.begin(), truth_group.end(), query_group.begin(), query_group.end(), std::back_inserter(v_intersection));
+                fn += v_intersection.size();
+                //if (!v_intersection.empty())
+                    //seqan3::debug_stream << truth_group << std::endl;
+                v_intersection.clear();
+                std::set_difference(query_group.begin(), query_group.end(), truth_group.begin(), truth_group.end(), std::back_inserter(v_intersection));
+                fp += v_intersection.size();
+                //if (!v_intersection.empty())
+                    //seqan3::debug_stream << truth_group << std::endl;
+                v_intersection.clear();
+            }
         }
     }
     spdlog::info("TP = {}, FN = {}, FP = {}", tp, fn, fp);
